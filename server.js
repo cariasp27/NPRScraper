96636001d0a2f87ac2e7e03c097f3738b1a56a8a
cariasp27 <paul.carias@yahoo.com>
2019-06-12 20:43:14 -0400

    updated mongoose connection

--- server.js
+++ server.js
@@ -28,7 +28,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/unit18Populater";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
